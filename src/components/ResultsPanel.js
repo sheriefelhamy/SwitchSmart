aa6a47bad4220282b0e9f3a544297409f2a72bb5
Ziad Maged <ziadmaged003@gmail.com>
2025-11-08 22:45:10 +0200

    added repo url to readme file

--- src/components/ResultsPanel.js
+++ src/components/ResultsPanel.js
@@ -1,5 +1,5 @@
 // src/components/ResultsPanel.jsx
-import React, {useRef} from "react";
+import React from "react";
 import html2canvas from "html2canvas";
 import { jsPDF } from "jspdf";
 import {
